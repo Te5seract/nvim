6d0257eebb9835130936f86fc721c57735891109
Isaac <isaacastley@live.com>
2022-03-29 22:17:20 +1100

    Delete init.lua

--- nvim/lua/init.lua
+++
@@ -1,9 +0,0 @@
-require("installed/packer");
-
-
---configs
-require("config/colorizer/colorizer-config")
-
-require("config/blankline/blankline-config")
-
-require("config/lsp-signature/lsp-signature-config")
