89975c25d68357765dfed9ea93b215e418cf749a
Isaac <isaacastley@live.com>
2022-03-29 21:58:12 +1100

    Add files via upload
    nvim config initial commit

---
+++ nvim/lua/init.lua
@@ -0,0 +1,9 @@
+require("installed/packer");
+
+
+--configs
+require("config/colorizer/colorizer-config")
+
+require("config/blankline/blankline-config")
+
+require("config/lsp-signature/lsp-signature-config")
