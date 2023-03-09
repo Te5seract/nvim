8b87b7135e69c7291fe0cbf00690552ad4ea944d
Te5seract <isaacastley@live.com>
2023-03-09 20:15:19 +1100

    added new leader shortcuts to switch between html and PHP file type

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -20,10 +20,14 @@ map("n", "<C-o>", ":NvimTreeToggle<cr>")
 map("n", "<leader>w", "<cmd>w<cr>")
 
 -- exit file
-map("n", "<leader>q", "<cmd>q<cr>")
+map("n", "<leader>qq", "<cmd>q<cr>")
 
 -- delete file buffer
 map("n", "<leader>xx", "<cmd>bd<cr>")
+
+-- switch file types
+map("n", "<leader>//", "<cmd>:set filetype=html<cr>")
+map("n", "<leader>??", "<cmd>:set filetype=php<cr>")
 
 -- end leader commands
 --/////////////////////////////////////////
